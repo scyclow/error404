118c6eb23258849d11b3c19b5b29eff73c117bb0
scyclow <spike716@gmail.com>
2016-04-12 16:29:10 -0400

    static

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
-import x from './utils/resizeWindow';
+import { sample, range, each } from 'lodash';
+import resize from './utils/resizeWindow';
+import createNoise from './utils/createNoise';
 
 const canvas = document.getElementById('canvas');
-console.log(x)
-x.onResize(x.setHeight);
-x.setHeight(window.innerWidth, window.innerHeight)
 
-function doSomething(ctx, a, b, c, adj) {
-  // if (a) ctx.fillRect(25*adj, 25*adj, 100*adj, 100*adj);
-  // if (b) ctx.clearRect(45*adj, 45*adj, 60*adj, 60*adj);
-  if (b) ctx.clearRect(0, 0, 800, 800);
-  if (c) ctx.strokeRect(50*adj, 50*adj, 50*adj, 50*adj);
+resize.onResize(resize.setHeight);
+resize.setHeight(window.innerWidth, window.innerHeight);
+
+// function randHex() {
+//   let color = '#';
+//   for (let i = 0; i < 6; i++) {
+//     color += random(16).toString(16);
+//   }
+//   return color.toUpperCase();
+// }
+
+const statSize = 5;
+
+function doSomething(ctx) {
+  let width = range(0, window.innerWidth, statSize);
+  let height = range(0, window.innerHeight, statSize);
+
+  each(width, x => {
+    each(height, y => {
+      ctx.fillStyle = sample(['#000000', '#ffffff']);
+      ctx.fillRect(x, y, statSize, statSize);
+    });
+  });
 }
 
 function draw(ctx) {
@@ -17,10 +34,10 @@ function draw(ctx) {
     doSomething(ctx,
       Math.random() > 0.5,
       Math.random() > 0.95,
-      Math.random() > 0.5,
-      Math.random() * 5.5
-    )
-  }, 1)
+      // Math.random() > 0.1
+      true
+    );
+  }, 10);
 }
 
 function withCanvas(fn) {
@@ -30,4 +47,5 @@ function withCanvas(fn) {
   }
 }
 
-withCanvas(draw)
+withCanvas(draw);
+createNoise();
